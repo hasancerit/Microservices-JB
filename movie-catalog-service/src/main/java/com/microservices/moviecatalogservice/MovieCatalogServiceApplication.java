fff53204049b283bbf5583be4d0252b7c64d8faa
hasancerit <hasan__cerit@hotmail.com>
2020-03-10 21:27:07 +0300

    Hystrix dashboard

--- movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced //Diğer servislere istek atarken, Discovery Server kullanılacağını söyler.
